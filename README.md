3b31ff3ae1b7c4d54aa9738bf98c56de388a1c8d
cirops <cirops@gmail.com>
2018-08-10 21:53:16 -0300

    simple jwt authorization with mock user

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # simple_node_server
+
+## The user can access both public routes (/api and /rules), but will only be able to access /secret if he logs in (/login)
+
+## Currently login is only a mock user.
+
+## To login, post request to /login, copy the json web token and add it (format: Bearer <token>) to your post request to /secret.
---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "This is working."
+  });
+});
+
+app.get("/api/rules", (req, res) => {
+  res.json({
+    message: "Gotta /login to know the /secret!"
+  });
+});
+
+app.post("/api/secret", verifyToken, (req, res) => {
+  jwt.verify(req.token, "thisisasecret", (err, authData) => {
+    if (err) {
+      res.json({
+        message: "You will never know the secret!"
+      });
+    } else {
+      res.json({
+        message: "You now know the secret.",
+        authData
+      });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "john",
+    email: "johndoe@gmail.com"
+  };
+
+  jwt.sign({ user }, "thisisasecret", { expiresIn: "1h" }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader != null) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.json({
+      message: "You will never know the secret!"
+    });
+  }
+}
+
+port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
